43dcffa624913e78989c3f7ff241cfc9d207061f
PaoloLopreiato <80921555+PaoloLopreiato@users.noreply.github.com>
2024-05-09 12:01:45 +0200

    Update main.py

--- Medium/2DProgramming/main.py
+++ Medium/2DProgramming/main.py
@@ -19,3 +19,16 @@ def memoization(r, c, rows, cols, cache):
                    memoization(r, c + 1, rows, cols, cache))   
     return cache[r][c]
 print(memoization(0, 0, 4, 4, [[0] * 4 for i in range(4)]))
+
+#True DP bottoms up 0(n * m)
+def dp(rows, cols):
+    prevRow = [0] * cols
+    
+    for i in range(rows - 1, -1, -1):
+        curRow = [0] * cols
+        curRow[cols - 1] = 1
+        for c in range(cols - 2, -1, -1):
+            curRow[c] = curRow[c + 1] + prevRow[c]
+        prevRow = curRow
+    return prevRow[0]
+print(dp(4, 4))
