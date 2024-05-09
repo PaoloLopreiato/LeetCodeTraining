08861a39348940a4c8c49448529e4da14ab67b68
PaoloLopreiato <80921555+PaoloLopreiato@users.noreply.github.com>
2024-05-09 12:07:32 +0200

    Update main.py

--- Medium/Leetcode_62/main.py
+++ Medium/Leetcode_62/main.py
@@ -1,3 +1,13 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        ...
+        def dp(rows, cols):
+            prevRow = [0] * cols
+            
+            for i in range(rows - 1, -1, -1):
+                curRow = [0] * cols
+                curRow[cols - 1] = 1
+                for c in range(cols - 2, -1, -1):
+                    curRow[c] = curRow[c + 1] + prevRow[c]
+                prevRow = curRow
+            return prevRow[0]
+        return dp(m, n)
