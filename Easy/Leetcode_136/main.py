ede0e6397861a3bc91dce9372abc1be4b62cbdba
PaoloLopreiato <80921555+PaoloLopreiato@users.noreply.github.com>
2024-05-27 10:53:17 +0200

    Update main.py

--- Easy/Leetcode_136/main.py
+++ Easy/Leetcode_136/main.py
@@ -3,4 +3,7 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ...#Maybe KUS?
+        res = 0
+        for n in nums:
+            res = n ^res
+        return res
